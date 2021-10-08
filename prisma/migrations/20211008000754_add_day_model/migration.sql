821d15f7aafb0ac56d96d2f34f738c289150c12e
bmviniciuss <bmvinicius11@gmail.com>
2021-10-07 21:16:32 -0300

    feat: add day model

---
+++ prisma/migrations/20211008000754_add_day_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Day" (
+    "id" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Day_pkey" PRIMARY KEY ("id")
+);
