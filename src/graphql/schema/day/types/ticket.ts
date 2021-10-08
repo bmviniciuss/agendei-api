b5135fb09433451085af3982d465206f20d2335e
bmviniciuss <bmvinicius11@gmail.com>
2021-10-08 01:18:47 -0300

    feat: add TIcket nexus model

---
+++ src/graphql/schema/day/types/ticket.ts
@@ -0,0 +1,50 @@
+import { TicketStatus } from '.prisma/client'
+
+import { objectType, list, nonNull, enumType } from 'nexus'
+
+import { Context } from '../../../../shared/infra/graphql/setupGraphql'
+import { UserNexus } from '../../user'
+import { SlotNexus } from './slot'
+
+export const TicketStatusNexusEnum = enumType({
+  name: 'TicketStatus',
+  members: Object.values(TicketStatus)
+})
+
+export const TicketNexus = objectType({
+  name: 'Ticket',
+  definition (t) {
+    t.nonNull.id('id')
+    t.nonNull.field('status', { type: TicketStatusNexusEnum })
+    t.nonNull.boolean('active')
+    t.field('user', {
+      type: UserNexus,
+      resolve (root, _args, { prisma }: Context) {
+        return prisma.ticket.findFirst({
+          where: {
+            id: root.id
+          },
+          select: {
+            user: true
+          }
+        }).then(data => data!.user)
+      }
+    })
+
+    t.nonNull.field('createdAt', { type: 'DateTime' })
+    t.nonNull.field('updatedAt', { type: 'DateTime' })
+    t.field('slots', {
+      type: list(nonNull(SlotNexus)),
+      resolve (root, _args, { prisma }: Context) {
+        return prisma.slot.findMany({
+          where: {
+            dayId: root.id
+          },
+          orderBy: {
+            startTime: 'asc'
+          }
+        })
+      }
+    })
+  }
+})
