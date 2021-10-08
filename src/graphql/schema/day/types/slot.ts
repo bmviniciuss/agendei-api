b78751f3ca1038b055cc9918c6d4f3f2646cd885
bmviniciuss <bmvinicius11@gmail.com>
2021-10-08 01:19:36 -0300

    feat: slots ticket resolvers

--- src/graphql/schema/day/types/slot.ts
+++ src/graphql/schema/day/types/slot.ts
@@ -1,4 +1,7 @@
-import { objectType } from 'nexus'
+import { list, objectType } from 'nexus'
+
+import { Context } from '../../../../shared/infra/graphql/setupGraphql'
+import { TicketNexus } from './ticket'
 
 export const SlotNexus = objectType({
   name: 'Slot',
@@ -7,6 +10,31 @@ export const SlotNexus = objectType({
     t.nonNull.int('usersLimit')
     t.nonNull.field('startTime', { type: 'DateTime' })
     t.nonNull.field('endTime', { type: 'DateTime' })
+    t.nonNull.field('activeTicketsCount', {
+      type: 'Int',
+      resolve (root, _args, { prisma }:Context) {
+        return prisma.ticket.count({
+          where: {
+            active: true,
+            slotId: root.id,
+            status: {
+              in: ['RESERVED', 'USED']
+            }
+          }
+        })
+      }
+    })
+    t.nonNull.field('tickets', {
+      type: list(TicketNexus),
+      resolve (root, _args, { prisma }:Context) {
+        return prisma.ticket.findMany({
+          where: {
+            active: true,
+            slotId: root.id
+          }
+        })
+      }
+    })
     t.nonNull.boolean('active')
     t.nonNull.field('createdAt', { type: 'DateTime' })
     t.nonNull.field('updatedAt', { type: 'DateTime' })
