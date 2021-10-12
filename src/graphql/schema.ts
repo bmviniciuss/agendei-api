599b4a9e5d39038112a2a3201c9a5d1d2e1de11c
bmviniciuss <bmvinicius11@gmail.com>
2021-10-12 00:52:24 -0300

    refactor: load modules automatically to create schema Fixes #23

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,31 +1,33 @@
 import { DateTimeResolver } from 'graphql-scalars'
 import {
-  makeSchema,
+  makeSchema as nexusMakeSchema,
   asNexusMethod
 } from 'nexus'
 import path from 'path'
 
-import * as DaySchema from './schema/day'
-import * as UserSchema from './schema/user'
+import { loadModulesSchemas } from './helpers/loadModulesSchemas'
 
 export const DateTime = asNexusMethod(DateTimeResolver, 'date')
 
-export const schema = makeSchema({
-  types: [
-    DateTime,
-    UserSchema,
-    DaySchema
-  ],
-  outputs: {
-    schema: path.join(__dirname, '/../../generated/schema.graphql'),
-    typegen: path.join(__dirname, '/../../generated/nexus.ts')
-  },
-  sourceTypes: {
-    modules: [
-      {
-        module: '@prisma/client',
-        alias: 'prisma'
-      }
-    ]
-  }
-})
+export async function makeSchema () {
+  const moduleSchemas = await loadModulesSchemas()
+  const schema = nexusMakeSchema({
+    types: [
+      DateTime,
+      ...moduleSchemas
+    ],
+    outputs: {
+      schema: path.join(__dirname, '/../../generated/schema.graphql'),
+      typegen: path.join(__dirname, '/../../generated/nexus.ts')
+    },
+    sourceTypes: {
+      modules: [
+        {
+          module: '@prisma/client',
+          alias: 'prisma'
+        }
+      ]
+    }
+  })
+  return schema
+}
