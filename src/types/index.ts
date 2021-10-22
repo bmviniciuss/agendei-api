79a999bb7ebff5867b4b807c2ed4f5d0116ddf08
bmviniciuss <bmvinicius11@gmail.com>
2021-10-21 22:10:24 -0300

    refactor: remove unused types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,4 @@
-import { Event, EventBooked, EventDetails, EventInstance, EventTypeEnum } from '@prisma/client'
-
-export interface Occurrence {
-  id: string
-  parentId: string
-  title: string
-  description: string
-  duration: number
-  slots: number
-  type: EventTypeEnum
-  active: boolean
-  date: string
-  createdAt: Date;
-  updatedAt: Date;
-}
+import { Event, EventBooked, EventDetails, EventInstance } from '@prisma/client'
 
 export type TEventWithDetails<T = Event> = T & {
   eventDetails: EventDetails;
@@ -23,11 +9,6 @@ export type EventWithDetails = TEventWithDetails<Event>
 export type EventBookedWithDetails = TEventWithDetails<EventBooked>
 
 export type EventInstanceWithEventDetails = TEventWithDetails<EventInstance>
-
-export type EventWithDetailsAndBookedEvents = EventWithDetails & {
-  eventsBooked: EventBookedWithDetails[]
-  eventsInstances: EventInstanceWithEventDetails[]
-}
 
 export type TimeRange = {
   startTime: Date
