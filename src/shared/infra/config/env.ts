d9d4d02c90cd4b90555fa6970f4bce8445722feb
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2021-11-11 18:25:01 -0300

    chore: chance APP_PORT to PORT

--- src/shared/infra/config/env.ts
+++ src/shared/infra/config/env.ts
@@ -2,6 +2,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  APP_PORT: 4000 || Number(process.env.APP_PORT),
+  APP_PORT: 4000 || Number(process.env.PORT),
   JWT_SECRET: process.env.JWT_SECRET || 'random_secret'
 }
