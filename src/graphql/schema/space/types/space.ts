64787800845f030517587e50e3025bdc7622fcc9
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2021-11-30 19:11:05 -0300

    fix: space occurrences listing same day bug

--- src/graphql/schema/space/types/space.ts
+++ src/graphql/schema/space/types/space.ts
@@ -1,5 +1,5 @@
-import { objectType, list, nonNull, arg, intArg } from 'nexus'
-import { resolve } from 'path/posix'
+import { isSameDay, endOfDay, startOfDay } from 'date-fns'
+import { objectType, list, nonNull, intArg } from 'nexus'
 
 import { PrismaEventRepository } from '../../../../modules/space/repos/implementations/PrismaEventRepository'
 import { ListOccurrences } from '../../../../modules/space/useCases/occurrence/listOccurrences/ListOccurrences'
@@ -44,14 +44,20 @@ export const SpaceNexus = objectType({
       },
       type: list(nonNull(OccurenceType)),
       async resolve ({ id }, { occurrencesInput }, { prisma }:Context) {
+        const dateRange = (() => {
+          const { startTime, endTime } = occurrencesInput
+          if (isSameDay(startTime, endTime)) {
+            return { startTime: startOfDay(startTime), endTime: endOfDay(endTime) }
+          }
+          return { startTime, endTime }
+        })()
+
         const eventRepository = new PrismaEventRepository(prisma)
         const useCase = new ListOccurrences(eventRepository)
+
         return useCase.execute({
           spaceIds: [id],
-          dateRange: {
-            startTime: occurrencesInput.startTime,
-            endTime: occurrencesInput.endTime
-          }
+          dateRange
         })
       }
     })
